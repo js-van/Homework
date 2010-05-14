6069fde400a3b6afb604f78142a315426e9d31be
mikola <mikola@sal-0019.(none)>
2010-05-13 21:25:29 -0500

    Problem 1 finished

--- MA715/hw5/mat.py
+++ MA715/hw5/mat.py
@@ -7,6 +7,9 @@ a4i = Symbol("a4i")
 a2j = Symbol("a2j")
 a4j = Symbol("a4j")
 
+X1  = Symbol("X1")
+Y1  = Symbol("Y1")
+
 J11 = Symbol("J11")
 J12 = Symbol("J12")
 J21 = Symbol("J21")
@@ -15,14 +18,26 @@ J22 = Symbol("J22")
 lam1 = Symbol("Lambda1")
 lam2 = Symbol("Lambda2")
 
-X = (J11 * lam1 + J12 * lam2)
-Y = (J21 * lam1 + J22 * lam2)
+X = (J11 * lam1 + J12 * lam2) + X1
+Y = (J21 * lam1 + J22 * lam2) + Y1
 
-Expr = a2i * a2j + (a4i * a2j + a2i * a4j) * Y + a4i * a4j * Y**2
+Q0 = a2i * a2j
+Q1 = a2i * a4j + a4i * a2j
+Q2 = a4i * a4j
+
+
+Expr = Q0 + Q1 * Y + Q2 * Y**2
 
 I1 = integrate(Expr, (lam1, 0, 1 - lam2))
 I2 = integrate(I1, (lam2, 0, 1))
 
-print I2
+#print collect(I2, (J21 + J22))
+
+
+JX = J21 + J22
+T = (Q0 + Y1 * (Q1 + Y1 * Q2) + JX / 3 * (Q1 + (2 * Y1 + JX / 2) * Q2)) / 2 - (J21 * J22 * Q2) / 12
+
+#print T
+print simplify(I2 - T)
 
 
--- MA715/hw5/prob1.py
+++ MA715/hw5/prob1.py
@@ -4,118 +4,108 @@ from scipy.linalg import *
 from scipy.sparse import *
 from scipy.linsolve import *
 from pylab import *
-import sympy as sp
-import sympy.abc as abc
-import sympy.integrals as spi
 
+#A quadrilateral element
 class QuadElement:
 	def __init__(self, ni, nx, ny):
 		self.ni = ni
 		self.nx = [nx[k] for k in ni]
 		self.ny = [ny[k] for k in ni]
 		M = matrix([ [ 1, nx[k], ny[k], nx[k] * ny[k]] for k in ni ])
-		self.alpha = M.inv()			
-	
+		self.alpha = inv(transpose(M))
+		
 	def laplacian(self):
 		res = []
-		for i in range(len(ni)):
-			for j in range(len(ni)):
-				ali = self.alpha[i,1:3]
-				alj = self.alpha[j,1:3]
+		for i in range(len(self.ni)):
+			for j in range(len(self.ni)):
+				ali = array(self.alpha[i,1:3]).flatten()
+				alj = array(self.alpha[j,1:3]).flatten()
 				ahi = self.alpha[i,3]
 				ahj = self.alpha[j,3]
+				Q0 = ali * alj
+				Q1 = ali * ahj + alj * ahi
+				Q2 = ahi * ahj
 				S = 0.
 				for k in range(2,4):
 					J = matrix([ [self.nx[p] - self.nx[0], self.ny[p] - self.ny[0]] for p in range(k-1,k+1) ])
-					X = array([J[0,0] + J[0,1], J[1,0] + J[1,1])
-					T = ali * alj + X * ((ali * ahj + alj * ahi) / 3. + X * ahi * ahj / 6.)
-					S += sum(T) / (2. * det(J))
-				res.append(((ni[i], nj[i]), S))
+					X = array([J[1,0] + J[1,1], J[0,0] + J[0,1]])
+					Y = array([self.ny[0], self.nx[0]])
+					T = Q0 + Y * (Q1 + Y * Q2) + X / 3. * (Q1 + (2 * Y + X / 2.) * Q2) - Q2 * array([J[1,0]*J[1,1], J[0,0]*J[0,1]]) / 12.
+					S -= sum(T) / (2. * det(J))
+				res.append(((self.ni[i], self.ni[j]), S))
 		return res
 	
-	'''
-	def rhs(self, f):
-		res = []
-		for i in range(len(ni)):
-			A = self.alpha[i,:]
-			S = 0.
-			for k in range(2,4):
-				J = matrix([ [self.nx[p] - self.nx[0], self.ny[p] - self.ny[0]] for p in range(k-1,k+1) ])
-				X = J[0,0] * abc.s + J[0,1] * abc.t
-				Y = J[1,0] * abc.s + J[1,1] * abc.t
-				F = f(X, Y)
-				PHI = A[0] + A[1] * X + A[2] * Y + A[3] * X * Y
-				S += sp.integrate(sp.integrate(F, (abc.s, 0, 1 - abc.t)), (abc.t, 0, 1)).n() / det(J)
-			res.append((ni[i], S))
-		return res
-	'''
-	
-def fe_solve(mesh, M, f, boundary, bvals):
+#Solves the finite element problem for the given mesh (which is just a list of elements)
+def fe_solve(mesh, nx, ny, f, boundary, bvals):
+	M = len(nx)
 	A = dok_matrix((M, M))
-	b = bvals * float(boundary)
-	
+	b = zeros((M))
 	for e in mesh:
 		for ((i,j), v) in e.laplacian():
 			if(boundary[i]):
 				continue
 			A[i,j] += v
-		for (i, v) in e.rhs(f):
-			if(boundary[i]):
-				continue
-			b[i] += v
-	
+	for i in range(M):
+		if(boundary[i]):
+			b[i] = bvals[i]
+			A[i,i] = 1
+		else:
+			b[i] = f(nx[i], ny[i])
 	return spsolve(A, b)
-
+	
 #Generates a rectangular regular grid mesh over the grid G
-def gen_regular_mesh(G):
-	#Get length
-	R, C, D = grid.shape
+def gen_regular_quad_mesh(grid):
+	D, R, C = grid.shape
 	M = R * C
 	def get_index(ix, iy):
 		if(ix < 0 or ix >= R or \
 		   iy < 0 or iy >= C):
 			return -1
-		return ix + R * iy
-
-	nx = grid[:,:,0].flatten()
-	ny = grid[:,:,1].flatten()
+		idx = ix + R * iy
+		return idx
+	nx = grid[0,:,:].flatten()
+	ny = grid[1,:,:].flatten()
 	mesh = []
 	for ix in range(R-1):
 		for iy in range(C-1):
-			mesh.append(Element(        \
-				[get_index(ix,   iy),   \ 
+			mesh.append(QuadElement(    \
+				[get_index(ix,   iy),   \
 				 get_index(ix+1, iy),   \
 				 get_index(ix+1, iy+1), \
 				 get_index(ix,   iy+1)],\
 				nx, ny))
-	return mesh, get_index
-
-
+	return mesh, nx, ny, M, R, C, get_index
 
 #Do the mesh generation
-xr = arange(-1:1:0.1)
-yr = arange(-1:1:0.1)
-grid, get_idx = mgrid(xr, yr)
-mesh, M = gen_regular_mesh(G)
+hx = 0.05
+hy = 0.05
+grid  = mgrid[-1:1+hx:hx,-1:1+hy:hy]
+mesh, nx, ny, M, R, C, get_idx = gen_regular_quad_mesh(grid)
 
 #Compute boundary conditions
 boundary = zeros((M), 'bool')
-for ix in range(len(xr)):
+for ix in range(C):
 	boundary[get_idx(ix,0)] = True
-	boundary[get_idx(ix,len(yr)-1)] = True
-for iy in range(len(yr)):
-	boundary[get_idx(0,ix)] = True
-	boundary[get_idx(len(xr)-1,iy)] = True
+	boundary[get_idx(ix,R-1)] = True
+for iy in range(R):
+	boundary[get_idx(0,iy)] = True
+	boundary[get_idx(C-1,iy)] = True
 bvals = zeros((M))
 
 #Construct f
 def f(x,y):
-	
+	if(sqrt(x*x + y*y) < 0.2):
+		return 100.
+	return 1.
 
 #Solve problem
-v = fe_solve(mesh, M
+u = fe_solve(mesh, nx, ny, f, boundary, bvals)
 
 #Display result
-	
-
+X = nx.reshape(R, C)
+Y = ny.reshape(R, C)
+U = u.reshape(R, C)
+pcolor(X, Y, U)
+savefig("prob1_result.png")
+show()
 
